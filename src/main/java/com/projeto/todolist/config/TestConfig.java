f679ad3294e1cbaa9442525459789bba26900e32
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-12 14:00:55 -0300

    Save test funcionarios

--- src/main/java/com/projeto/todolist/config/TestConfig.java
+++ src/main/java/com/projeto/todolist/config/TestConfig.java
@@ -1,10 +1,13 @@
 package com.projeto.todolist.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projeto.todolist.entities.Funcionario;
 import com.projeto.todolist.repositories.FuncionarioRepository;
 import com.projeto.todolist.repositories.TarefaRepository;
 
@@ -21,7 +24,14 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		Funcionario func1 = new Funcionario(null,"Rodrigo","Dev");
+		Funcionario func2 = new Funcionario(null,"João","CEO");
+		Funcionario func3 = new Funcionario(null,"Maria","Gerente");
 		
+		funcionarioRepository.saveAll(Arrays.asList(func1,func2,func3));
+		
+		
+		                                   
 	}
 
 }

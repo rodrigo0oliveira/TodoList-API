f4da27d53c924a75a9f6ea189570d60879a4bff1
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-12 13:45:48 -0300

    Implements Class config for test

--- src/main/java/com/projeto/todolist/config/TestConfig.java
+++ src/main/java/com/projeto/todolist/config/TestConfig.java
@@ -1,5 +1,27 @@
 package com.projeto.todolist.config;
 
-public class TestConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.projeto.todolist.repositories.FuncionarioRepository;
+import com.projeto.todolist.repositories.TarefaRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private TarefaRepository tarefaRepository;
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
 
 }
